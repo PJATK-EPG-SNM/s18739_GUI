9d6dff1c6ee8c5a7672fe63b9cdad3179814fcd4
s18739 <61964983+s18739@users.noreply.github.com>
2020-05-04 16:44:26 +0200

    GUI

---
+++ GUI/Assets/Scripts/Zbierable/Coin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour{
+
+    //private Animator anim;
+    private float animationTime = 0;
+    private ScoreManager ScoreManager;
+    private bool collected;
+    public int points = 1;
+
+    void Start(){
+    ScoreManager = GameObject.Find("ScoreManagerobject").GetComponent<ScoreManager>();
+    collected = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Player")&& !collected){
+            collected = true;   
+            ScoreManager.IncrementScore(points);
+            //anim.SetTrigger("collected");
+            Invoke("CollectCoin", animationTime);
+        }
+    }
+
+    private void CollectCoin(){
+        Destroy (gameObject);
+    }
+}
